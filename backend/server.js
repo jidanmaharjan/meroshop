28497df89c943bce3183bbda30a85868ce92b735
Jidan Maharjan <jidanmaharjan1@gmail.com>
2022-09-08 16:35:00 +0545

    server delete

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
-const server = app.listen(process.env.PORT || 4000, ()=>{
+app.listen(process.env.PORT || 4000, ()=>{
     console.log(`Server started on PORT : ${process.env.PORT || 4000} in ${process.env.NODE_ENV} mode.`);
 })
 
