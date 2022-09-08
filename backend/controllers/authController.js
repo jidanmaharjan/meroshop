ec3072efd4d52fe07adeabfcf78c8d0e16995c5b
Jidan Maharjan <jidanmaharjan1@gmail.com>
2022-09-09 01:08:07 +0545

    fixed error handler and  im an idiot

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require('../models/user')
 
-const ErrorHandler = require('../utils/errorhandler');
+const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
