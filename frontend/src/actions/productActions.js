7cf901375fadedad54f755c6eb163224cff3cedd
Jidan Maharjan <jidanmaharjan1@gmail.com>
2022-09-09 01:41:02 +0545

    api update

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -37,10 +37,10 @@ import {
             type: ALL_PRODUCTS_REQUEST
         })
 
-        let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&ratings[gte]=${rating}`
+        let link = `https://meroshopp.herokuapp.com/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&ratings[gte]=${rating}`
 
         if(category){
-            link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&category=${category}&ratings[gte]=${rating}`
+            link = `https://meroshopp.herokuapp.com/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&category=${category}&ratings[gte]=${rating}`
         }
 
         const { data } = await axios.get(link)
