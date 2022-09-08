f9f7d212a7bfaadde798621f3f7f179b465d9d71
Jidan Maharjan <jidanmaharjan1@gmail.com>
2022-09-09 01:54:07 +0545

    fixed all

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(fileUpload());
 //         )
 //     })
 // }
-if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'PRODUCTION'){
     app.use(express.static('../frontend/build/'))
     app.get('*',(req, res)=>{
         res.sendFile(path.resolve(__dirname, '../frontend/build/index.html')
